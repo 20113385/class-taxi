221f7af62706628d26869c2a9b45bb963f01e82e
Indah Mega Lestari <80435571+20113385@users.noreply.github.com>
2021-03-18 08:47:50 -0300

    Add files via upload

--- class taxi.cs
+++ class taxi.cs
@@ -1,20 +1,29 @@
-class program
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pemrogaman_taxi_0
 {
-    static void Main(string[]args)
+    class taxi
     {
-        // membuat objek Taxi
-        Taxi taxi = new Taxi();
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassanger { get; set; }
 
-        // pengesetan nilai properties
-        taxi.DriverName = "jono";
-        taxi.OnDuty = true;
-        taxi.NumPassenger = 10;
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty);
+            Console.WriteLine("Number of Passanger : {0}", NumPassanger);
 
-        // pemanggilan methord
-        taxi.TaxiInfo();
-        taxi.PickUpPassenger();
-        taxi.DropOffPassenger();
-
-        Console.ReadKey();
+        }
+        public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+        }
+        public void DropOfPassanger()
+        {
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+        }
     }
 }
